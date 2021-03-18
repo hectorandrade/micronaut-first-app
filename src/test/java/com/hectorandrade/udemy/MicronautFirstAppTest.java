2c0796bee7c9eb1286fb13be739a57cf64dcc844
Hector-Andrade <hectorandrade>
2021-03-18 23:16:39 +0000

    Initial commit

---
+++ src/test/java/com/hectorandrade/udemy/MicronautFirstAppTest.java
@@ -0,0 +1,21 @@
+package com.hectorandrade.udemy;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class MicronautFirstAppTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
